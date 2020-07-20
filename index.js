b74865a6987826828566f6055599f5569e17805c
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-20 17:05:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(name => name.toLowerCase() == string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(name => name.startsWith(string))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(obj => obj.name === string)
+}
