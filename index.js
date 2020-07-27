3756cc28b443b8147610d55e7440fee5dab11ac1
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-26 22:22:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(function(filter_item) {
+        return filter_item.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (filter_item) {
+        return filter_item.charAt(0) === string.charAt(0)
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function (filter_item) {
+        return filter_item.name === string
+    })
+}
